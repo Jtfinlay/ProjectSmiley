ee3c8fce3dd316f0f5d651515c20dcf09f2f74d0
Jacob Viau <jviau@ualberta.ca>
2014-11-24 14:00:04 -0700

    Stupid mistakes

--- imageDriver.m
+++ imageDriver.m
@@ -3,7 +3,7 @@ files = dir("Data/*.jpg");
 names = {files.name}';
 
 for i=1:size(names,1)
-	images = splitImages(strcat("Data/", names{1}));
+	images = splitImages(strcat("Data/", names{i}));
 	% Next step is James' or Steven's?
 	for j=1:size(images,1)
 		for k=1:size(images,2)
