e5213deff40424739ff7796fdf149d49a5f807a8
Jacob Viau <jviau@ualberta.ca>
2014-11-24 13:37:38 -0700

    Initial driver

---
+++ imageDriver.m
@@ -0,0 +1,14 @@
+
+files = dir("Data/*.jpg");
+names = {files.name}';
+
+for i=1:size(names,1)
+	images = splitImages(strcat("Data/", names{1}));
+	% Next step is James' or Steven's?
+	for j=1:size(images,1)
+		for k=1:size(images,2)
+			img = images{j,k};
+			% Do something with img
+		end
+	end
+end
