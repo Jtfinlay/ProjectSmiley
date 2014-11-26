24ca58746f5143e492e57f4fd5e35983a28e9dbe
Steven <sgerdes@ualberta.ca>
2014-11-25 22:09:58 -0700

    Made image_transformations file
    This is a rename of my first file removing things unrelated to image transformations and replacing the one helper function
    with two helper functions
    all_training_transformations
    and
    all_testing_transformations

--- image_transformations.m
+++ image_transformations.m
@@ -1,17 +1,5 @@
-pkg load all
-
-
-function img = new_img(image, name)
-   img = struct('img',image,'name', name);   
-return
-end
-
-function print_imgs(images)
-	for j = 1:size(images,2)
-		imwrite(images(j).img, sprintf("./output/%s.png", images(j).name));
-	end
-return
-end
+%This file uses imperspectivewarp to make multiple different transformations on images
+%http://octave.sourceforge.net/image/function/imperspectivewarp.html
 
 %input: an array of images.
 %output: an larger array of images with each rotation from 0 to 345 degrees using 15 degree increments 
@@ -21,7 +9,7 @@ function rotated = all_rotations(images)
 	for j = 1:size(images,2)
 	for deg = 0:pi/6:(2*pi - pi/6)
 		R = [cos(deg) sin(deg); -sin(deg) cos(deg)];
-		rotated(index++) = new_img(imperspectivewarp(images(j).img, R, :, "loose", 255), sprintf("%s_rot%f",images(j).name, deg));
+		rotated(index++) = new_img(imperspectivewarp(images(j).img, R, :, "loose", 255), images.class, sprintf("%s_rot%f",images(j).name, deg));
 	end
 	end
 return
@@ -36,7 +24,7 @@ function flipped = all_flips(images)
 	for j = 1:size(images,2)
 		flipped(index++) = images(j);
 		R = diag([-1, 1, 1]);
-		flipped(index++) = new_img(imperspectivewarp(images(j).img, R, :, "loose", 255), sprintf("%s_flip",images(j).name));
+		flipped(index++) = new_img(imperspectivewarp(images(j).img, R, :, "loose", 255), images.class, sprintf("%s_flip",images(j).name));
 	end
 return
 end
@@ -48,10 +36,10 @@ function skewed = all_skews(images)
 	index = 1;
 	for j = 1:size(images,2)
 		R = [cos(0) sin(0.785); -sin(0) cos(0)];
-		skewed(index++) = new_img(imperspectivewarp(images(j).img, R, :, "loose", 255), sprintf("%s_Hskew%f",images(j).name));
+		skewed(index++) = new_img(imperspectivewarp(images(j).img, R, :, "loose", 255), images.class, sprintf("%s_Hskew%f",images(j).name));
 
 		R = [cos(0) sin(0); -sin(0.785) cos(0)];
-		skewed(index++) = new_img(imperspectivewarp(images(j).img, R, :, "loose", 255), sprintf("%s_Vskew%f",images(j).name));
+		skewed(index++) = new_img(imperspectivewarp(images(j).img, R, :, "loose", 255), images.class, sprintf("%s_Vskew%f",images(j).name));
 		
 	end
 return
@@ -59,28 +47,28 @@ end
 
 %input: an array of images.
 %output: an larger array of images. For each image there is the original, a wide one and a skinny one.
-function streched = all_streches(images)
+function streched = all_stretches(images)
 	index = 1;
 	for j = 1:size(images,2)
 		streched(index++) = images(j);
 		
 		R = diag([0.5, 1, 1]);
-		streched(index++) = new_img(imperspectivewarp(images(j).img, R, :, "loose", 255), sprintf("%s_skinny",images(j).name));
+		streched(index++) = new_img(imperspectivewarp(images(j).img, R, :, "loose", 255), images.class, sprintf("%s_skinny",images(j).name));
 		
 		R = diag([1, 0.5, 1]);
-		streched(index++) = new_img(imperspectivewarp(images(j).img, R, :, "loose", 255), sprintf("%s_wide",images(j).name));
+		streched(index++) = new_img(imperspectivewarp(images(j).img, R, :, "loose", 255), images.class, sprintf("%s_wide",images(j).name));
 	end
 return
 end
 
-function transformations = all_transformations(images)
-	transformations = all_streches(all_flips(all_skews(all_rotations(images))));
+function transformations = all_training_transformations(images)
+	transformations = all_stretches(all_flips(all_skews(all_rotations(images))));
 return
 end
   
-%http://octave.sourceforge.net/image/function/imperspectivewarp.html
+function transformations = all_testing_transformations(images)
+	transformations = allTrainingTransformations(all_rotations(images))));
+return
+end
 
-img = imread ("./smile.png");
-images = all_transformations([new_img(img,"orignial")]))));
-print_imgs(images);
 
