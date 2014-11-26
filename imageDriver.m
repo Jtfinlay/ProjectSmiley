298517b9a3687ecb7431df7a1badc7f861cb28bc
Steven <sgerdes@ualberta.ca>
2014-11-25 23:09:19 -0700

    Made it so the image driver uses my transformation code.
    It also contains a struct which defines an image. I assume we need the
    class so that has been added to the struct

--- imageDriver.m
+++ imageDriver.m
@@ -1,14 +1,53 @@
+pkg load all
+source('./image_transformations.m');
+% I had asked if we want to pair more data to our images. I was just told how the images where loaded
+% Which I guess means no one cares if I add more data to the images
+% So currently I am using my simple struct which is define as below
+%If there is any problem with this feel free to change it or ask me to.
+function img = new_image(image, class, name)
+   img = struct('img',image,... %octaves normal representation of an image
+		'class', class,... %class name i.e. :) or whatever
+		'name', name);  % the modifications done to the image 
+						% along with its original file name and  
+						% incidences (this is for debugging only)
+return
+end
 
-files = dir("Data/*.jpg");
-names = {files.name}';
 
-for i=1:size(names,1)
-	images = splitImages(strcat("Data/", names{i}));
-	% Next step is James' or Steven's?
-	for j=1:size(images,1)
-		for k=1:size(images,2)
-			img = images{j,k};
-			% Do something with img
+function index = getClassIndex(fileName)
+ index =  str2num(regexprep (fileName, '\D+', '$1 '));
+return
+end
+
+%loads all images from each file, then does all transformations to them
+%this may load a lot of data into memory at once and so maybe it wasn't smart 
+%to make this a helper function but we will see
+function images = get_all_images()
+	class_names = {':)',':-)',':(', ':-(', ':D', ':-D', ':|', ':-|', ':P', ':-P', ':O', ':-O', 
+					'8)', '8-)','8(', '8-(', '8D', '8-D', '8|', '8-|', '8P', '8-P', '8O', '8-O', 
+					';)', ';-)', ';(', ';-(', ';D', ';-D', ';|', ';-|', ';P', ';-P', ';O', ';-O'};
+	images = [];
+	files = dir("Data/*.jpg");
+	names = {files.name};
+	for i=1:size(names,1)
+		class_index = getClassIndex(names(i){1});
+		orignal_images = splitImages(strcat("Data/", names{i}));
+		for j=1:size(orignal_images,1)
+			for k=1:size(orignal_images,2)
+				images = [ images, all_training_transformations([new_image(orignal_images{j,k}, class_names{class_index}, sprintf("$s_%d_%d", names{i}, j,k))]) ];
+			end
 		end
 	end
+	return
 end
+
+
+images = get_all_images();
+%for i=1:size(images, 2)
+	%preprocessImage(images(i).img);
+	%learnImage() or whatever
+%end
+
+
+
+
